963b10153c52ffc6299ecaa5e9e5b597011b1b4d
Jalpesh Borad <jalpeshborad@gmail.com>
2020-02-24 12:52:12 +0530

    Added handle for Ctrl+C trigger

--- scripts/streaming/dataframe/network_word_count.py
+++ scripts/streaming/dataframe/network_word_count.py
@@ -7,12 +7,13 @@ __email__ = "jalpeshborad@gmail.com"
 from pyspark.sql.functions import explode, split
 
 from base.context import get_spark_session
-from base.decorators import time_taken
+from base.decorators import time_taken, catch_keyboard_interrupt
 
 spark = get_spark_session("NetworkWordCount")
 host, port = "localhost", 8000
 
 
+@catch_keyboard_interrupt
 @time_taken
 def main():
     """Legacy park streaming"""
